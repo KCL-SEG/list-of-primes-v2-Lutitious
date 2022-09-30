64713f813051dd80b9c82b25182abac09aef3815
Rahul Imran <notlutitious@gmail.com>
2022-09-30 21:10:47 +0100

    Feat: Raise ValueError when number_of_primes < 1

--- primes.py
+++ primes.py
@@ -3,4 +3,14 @@
 
 def primes(number_of_primes):
     list = []
+    i = 2
+    if number_of_primes < 1:
+        raise ValueError
+    while len(list) < number_of_primes:
+        for j in range(2, i):
+            if i % j == 0:
+                break
+        else:
+            list.append(i)
+        i += 1
     return list
